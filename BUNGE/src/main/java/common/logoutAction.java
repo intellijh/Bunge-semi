dd2b972771a6bb63c56f48a5fc5fe227bc2efd94
Jeonghyo <129913158+kjh936@users.noreply.github.com>
2024-03-28 09:55:08 +0900

    Update logoutAction.java

--- BUNGE/src/main/java/common/logoutAction.java
+++ BUNGE/src/main/java/common/logoutAction.java
@@ -26,24 +26,3 @@ public class logoutAction implements Action {
 	}
 
 }
-=======
-package common;
-
-import java.io.IOException;
-
-import common.action.Action;
-import common.action.ActionForward;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-public class logoutAction implements Action {
-
-	@Override
-	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-}
