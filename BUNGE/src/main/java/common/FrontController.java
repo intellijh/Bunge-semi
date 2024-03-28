e91d04ffc9c9b236c6e8d5344c86beff7ea10ebf
DESKTOP-KK1OG1M\jhta <kjh4940@naver.com>
2024-03-28 17:38:32 +0900

    채팅

--- BUNGE/src/main/java/common/FrontController.java
+++ BUNGE/src/main/java/common/FrontController.java
@@ -3,6 +3,8 @@ package common;
 import java.io.IOException;
 
 import chat.ChatListLoadAction;
+import chat.message.MessageLoadAction;
+import chat.message.MessageSendAciton;
 import common.action.Action;
 import common.action.ActionForward;
 import jakarta.servlet.RequestDispatcher;
@@ -16,9 +18,9 @@ import chat.ChatListAction;
 @WebServlet("*.com")
 public class FrontController extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doProcess(HttpServletRequest request,
+    protected void doProcess(HttpServletRequest request,
                              HttpServletResponse response) throws ServletException, IOException {
 
         String RequestURI = request.getRequestURI();
@@ -35,49 +37,49 @@ public class FrontController extends HttpServlet {
 
         switch (command) {
             //로그인
-            case "/login.com" :
+            case "/login.com":
                 action = new loginAction();
                 break;
-            case "/loginProcess.com" :
+            case "/loginProcess.com":
                 action = new loginProcessAction();
                 break;
             //로그아웃
-            case "/logout.com" :
+            case "/logout.com":
                 action = new logoutAction();
                 break;
-            case "/join.com" :
-	        	action = new joinAction();
-	        	break;
-	       case "/joinProcess.com" :
-	    	   action = new joinProessAction();
-	    	   break;
-	    	   //아이디 중복 검사
-	       case "/idcheck.com" :
-	    	   action = new idcheckAction();
-	    	   break;
-	    	   //닉네임 중복 검사
-	       case"/nickcheck.com" :
-	    	   action = new nickcheckAction();
-	    	   break;
-	    	   //아이디 찾기
-	       case "/findid.com" :
-	    	   action = new findidAction();
-	    	   break;
-	       case"/findidProcess.com" :
-	    	   action = new findidProcessAction();
-	    	   break;
+            case "/join.com":
+                action = new joinAction();
+                break;
+            case "/joinProcess.com":
+                action = new joinProcessAction();
+                break;
+            //아이디 중복 검사
+            case "/idcheck.com":
+                action = new idcheckAction();
+                break;
+            //닉네임 중복 검사
+            case "/nickcheck.com":
+                action = new nickcheckAction();
+                break;
+            //아이디 찾기
+            case "/findid.com":
+                action = new findidAction();
+                break;
+            case "/findidProcess.com":
+                action = new findidProcessAction();
+                break;
             //게시판에 글 추가
-            case "/InfoAdd.com" :
+            case "/InfoAdd.com":
                 action = new InfoAddAction();
                 break;
             //글쓰기
-            case "/Write.com" :
+            case "/Write.com":
                 action = new InfoWriteAction();
                 break;
-            case "/BoardList.com" :
+            case "/BoardList.com":
                 action = new BoardListAction();
                 break;
-            case "/BoardDetail.com" :
+            case "/BoardDetail.com":
                 action = new BoardDetailAction();
                 break;
             //채팅 페이지 이동
@@ -87,6 +89,14 @@ public class FrontController extends HttpServlet {
             //채팅 리스트 불러오기
             case "/chatLoad.com":
                 action = new ChatListLoadAction();
+                break;
+            //채팅 리스트 불러오기
+            case "/messageLoad.com":
+                action = new MessageLoadAction();
+                break;
+            //채팅 리스트 불러오기
+            case "/messageSend.com":
+                action = new MessageSendAciton();
                 break;
         } //switch (command)
 
