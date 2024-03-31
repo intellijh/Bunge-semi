62f89176f5bc2dcb5f267a7ca01caa9c2f0ab77b
kjh <kjh4940@naver.com>
2024-03-31 20:09:09 +0900

    chat.js 코드 및 주석 정리

--- BUNGE/src/main/webapp/js/chat/chat.js
+++ BUNGE/src/main/webapp/js/chat/chat.js
@@ -1,6 +1,7 @@
 let chatId = 0;
 
 function getChatList() {
+
     console.log("getList");
     $.ajax({
         type: "post",
@@ -54,12 +55,6 @@ function getChatList() {
                 chatId = $(".contacts li").eq(0).attr("id");
             }
             console.log("stored chatId = " + chatId);
-            // $(".contacts li").click(function () {
-            //     chatId = $(this).attr("id");
-            //     console.log("click chatId = " + chatId);
-            // });
-
-
 
             $(".contacts li").removeClass("active");
             $("#"+chatId).addClass("active");
@@ -75,93 +70,11 @@ $(document).on('click', '.contacts li', function() {
     getChatList();
 });
 
-// getMessage = setInterval(function () {
-//
-//     $.ajax({
-//         type: "post",
-//         url: "messageLoad.com",
-//         data: {"loginId": $("#loginId").val()},
-//         dataType: "json",
-//         success: function (rdata) {
-//             console.log("ajax");
-//             if (rdata.chat.length > 0) {
-//                 console.log("0over?");
-//                 let output = ``;
-//                 $(rdata.chat).each(function () {
-//                     console.log(rdata.chat);
-//                     output +=
-//                         `<li>
-//                             <div class="d-flex bd-highlight">
-//                                 <div class="img_cont">
-//                                     <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg"
-//                                          class="rounded-circle user_img">
-//                                     <span class="online_icon"></span>
-//                                 </div>
-//                                 <div class="user_info">
-//                                     <span>${this.sellerId}</span>
-//                                     <p>${this.buyerId}</p>
-//                                     <p>${this.openDate}</p>
-//                                 </div>
-//                             </div>
-//                         </li>`;
-//                 });
-//                 $(".contacts").html(output);
-//             }
-//         }
-//     });
-// }, 3000);
-
-function sendMessage() {
-    let msg = $(".type_msg").val();
-    console.log(msg);
-    if (msg == null) {
-        return;
-    }
-
-    // $.ajax({
-    //     type: "post",
-    //     url: "chatLoad.com",
-    //     data: {"loginId": $("#loginId").val()},
-    //     dataType: "json",
-    //     success: function (rdata) {
-    //         console.log("ajax");
-    //         if (rdata.chat.length > 0) {
-    //             console.log("0over?");
-    //             let output = ``;
-    //             $(rdata.chat).each(function () {
-    //                 console.log(rdata.chat);
-    //                 output +=
-    //                     `<li>
-    //                         <div class="d-flex bd-highlight">
-    //                             <div class="img_cont">
-    //                                 <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg"
-    //                                      class="rounded-circle user_img">
-    //                                 <span class="online_icon"></span>
-    //                             </div>
-    //                             <div class="user_info">
-    //                                 <span>${this.sellerId}</span>
-    //                                 <p>${this.buyerId}</p>
-    //                                 <p>${this.openDate}</p>
-    //                             </div>
-    //                         </div>
-    //                     </li>`;
-    //             });
-    //             $(".contacts").html(output);
-    //         }
-    //     }
-    // });
-}
-
 $(function () {
     $('#action_menu_btn').click(function () {
         $('.action_menu').toggle();
     });
     console.log("load");
-
-    // $(".send_btn").click(function () {
-    //     console.log("click");
-    //     sendMessage();
-    // });
 
     getChatList();
 });
